600ca693ea9680989d0db7d7b1b5e606fac5c3e9
Dominic Kendrick <dominic.kendrick@guardian.co.uk>
2014-05-07 10:05:10 +0100

    adding limit model

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,26 @@
 var brokeApp = angular.module('brokeApp', []);
 
-brokeApp.controller('TransactionsListCtrl', ['$scope', '$http',
+brokeApp.controller('TransactionsListCtrl', ['$scope','$rootScope', '$http',
   function($scope, $http) {
     $http.get('data/transactions.json').success(function(data) {
       $scope.transactions = data;
+      $rootScope.$broadcast('UPDATE_TRANSACTION_DATA', $scope.transactions);
     });
     $scope.orderProp = 'creationDate';
 }]);
+
+brokeApp.controller('LimitsListCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('data/limits.json').success(function(data) {
+      $scope.limits = data;
+    });
+    $scope.$on('UPDATE_TRANSACTION_DATA', function ( event, transactions ) { 
+      
+    });
+    $scope.orderProp = 'creationDate';
+    $scope
+}]);
+
+var checkLimit = function(limit, transaction){
+  
+}
